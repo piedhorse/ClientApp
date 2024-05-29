4142ef822727954f295b52bcb3c65531e3f576d9
umut <96746943+piedhorse@users.noreply.github.com>
2024-05-29 21:36:22 +0300

    Add files via upload

---
+++ home.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  userForm: FormGroup = new FormGroup({});
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.initialzeForm();
+
+  }
+  initialzeForm() {
+    this.userForm = this.formBuilder.group(
+      {
+        name: ['', Validators.required, Validators.minLength(3), Validators.maxLength(15)]
+      }
+    )
+  }
+  submitForm() {
+    this.submitted = true;
+
+
+    if (this.userForm.valid) {
+      console.log(this.userForm.value)
+
+    }
+  }
+}
